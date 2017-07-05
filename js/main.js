a42c9cfa8e0c22a23ac0a562379ad09dd2acbc2a
Rey Marquez <reymarquez25@gmail.com>
2017-07-05 10:17:08 -0700

    Css Done

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(function() {
+
+let spin;
+let reel;
+
+reel = [
+    reel1['cherry', 'orange', 'banana', 'bar', 'bar2', 'bar3' ],
+    reel2['cherry', 'orange', 'banana', 'bar', 'bar2', 'bar3' ],
+    reel3['cherry', 'orange', 'banana', 'bar', 'bar2', 'bar3' ],
+]
+
+$('button').on('click', function (evt) {
+    return console.log(evt)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
