e74963a758eac78a3ec7cc718274b6fc56c62bbf
Rey Marquez <reymarquez25@gmail.com>
2017-07-05 11:54:55 -0700

    Slot reel working

--- js/main.js
+++ js/main.js
@@ -1,17 +1,42 @@
 $(function() {
 
-let spin;
-let reel;
+function spin (){
+    var reel1;
+    var reel2;
+    var reel3;
 
-reel = [
-    reel1['cherry', 'orange', 'banana', 'bar', 'bar2', 'bar3' ],
-    reel2['cherry', 'orange', 'banana', 'bar', 'bar2', 'bar3' ],
-    reel3['cherry', 'orange', 'banana', 'bar', 'bar2', 'bar3' ],
-]
+var icons = 
+["/Users/reymarquez/Documents/WDI/scratch/slots/images/71.png",
+ "/Users/reymarquez/Documents/WDI/scratch/slots/images/banana1.png",
+ "/Users/reymarquez/Documents/WDI/scratch/slots/images/ba1.png",
+ "/Users/reymarquez/Documents/WDI/scratch/slots/images/cherry1.png" ]
+
+     reel1 = Math.floor(Math.random() * (4-1+1) +1);
+     reel2 = Math.floor(Math.random() * (4-1+1) +1);
+     reel3 = Math.floor(Math.random() * (4-1+1) +1);
+
+     $($('#reel1').html('<img src = "' + icons[reel1-1] + '">'));
+     $($('#reel2').html('<img src = "' + icons[reel2-1] + '">'));
+     $($('#reel3').html('<img src = "' + icons[reel3-1] + '">'));
+
+     
+$('button').on('click', function (evt) {
+    spin();
+});
+
+
+
+
+
+
+
+
+
+}
 
 $('button').on('click', function (evt) {
-    return console.log(evt)
-})
+    spin();
+});
 
 
 
