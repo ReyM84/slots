c7e25f6bfb8aa5b9de263a581e0677ba4bd1c13c
Rey Marquez <reymarquez25@gmail.com>
2017-07-06 14:57:07 -0700

    final logic

--- js/main.js
+++ js/main.js
@@ -1,65 +1,43 @@
 var credit = 20;
 $(function() {
+	var icons = ["images/71.png", "images/banana1.png", "images/ba1.png", "images/cherry1.png", "images/watermelon.png", "images/prune.png", "images/lemon.png", "images/orange.png", "images/winner.png"];
+	var reel1;
+	var reel2;
+	var reel3;
 
-function spin (){
-    var reel1;
-    var reel2;
-    var reel3;
- 
-
-var icons = 
-["images/71.png",
- "images/banana1.png",
- "images/ba1.png",
- "images/cherry1.png",
- "images/watermelon.png",
- "images/prune.png",
- "images/lemon.png",
- "images/orange.png",
- "images/winner.png" ]
-
-     reel1 = Math.floor(Math.random() * (9-1+1) +1);
-     reel2 = Math.floor(Math.random() * (9-1+1) +1);
-     reel3 = Math.floor(Math.random() * (9-1+1) +1);
-
-     
-
-     $($('#reel1').html('<img src = "' + icons[reel1-1] + '">'));
-     $($('#reel2').html('<img src = "' + icons[reel2-1] + '">'));
-     $($('#reel3').html('<img src = "' + icons[reel3-1] + '">'));
-
-
-    if ( reel1 === reel2 && reel2 === reel3 && reel1 === 9 ) {
-        credit += 100
-     } else if ( reel1 === reel2 && reel2 === reel3 && reel1 === 3) {
-         credit += 50
-     } else if ( reel1 === reel2 && reel2 === reel3 ) {
-         credit += 30    
-     } else if ( reel1 === reel2 || reel2 === reel3 || reel1 === reel3){
-         credit += 5
-     } else {
-         credit -= 5
-     };
-
-     if ( credit < 1){
-         credit = 20, 
-         $('#gameover').html('GAME OVER')
-     } else {
-         $('#gameover').html('')
-     };
-
-$('#score').html(credit)
-
-};
-
-$('button').on('click', function (evt) {
-    spin();
-    
-
-});
-
-
-
-
-
+	function spin(a, b, c, d, e) {
+		reel1 = Math.floor(Math.random() * (9 - 1 + 1) + 1);
+		reel2 = Math.floor(Math.random() * (9 - 1 + 1) + 1);
+		reel3 = Math.floor(Math.random() * (9 - 1 + 1) + 1);
+		$($('#reel1').html('<img src = "' + icons[reel1 - 1] + '">'));
+		$($('#reel2').html('<img src = "' + icons[reel2 - 1] + '">'));
+		$($('#reel3').html('<img src = "' + icons[reel3 - 1] + '">'));
+		if (reel1 === reel2 && reel2 === reel3 && reel1 === 9) {
+			credit += a
+		} else if (reel1 === reel2 && reel2 === reel3 && reel1 === 3) {
+			credit += b
+		} else if (reel1 === reel2 && reel2 === reel3) {
+			credit += c
+		} else if (reel1 === reel2 || reel2 === reel3 || reel1 === reel3) {
+			credit += d
+		} else {
+			credit -= e
+		};
+		if (credit < 1) {
+			credit = 20,
+				$('#gameover').html('GAME OVER')
+		} else {
+			$('#gameover').html('')
+			$('#score').html(credit)
+		}
+	};
+	$('#minbtn').on('click', function(evt) {
+		spin(20, 15, 10, 5, 1);
+	});
+	$('#spinbtn').on('click', function(evt) {
+		spin(100, 50, 30, 10, 5);
+	});
+	$('#maxbtn').on('click', function(evt) {
+		spin(400, 100, 50, 25, 10);
+	});
 });
